82c032a211fac1b4d041a23eb7b3b2345a414445
Osben Toulson <otoulson@dcscorp.com>
2025-12-17 02:08:01 -0500

    Enhance UI: Bold and adjust styles for 'Upload Image' and 'Customize Text' labels

--- src/ui/components/App.css.js
+++ src/ui/components/App.css.js
@@ -6,4 +6,13 @@ export const style = css`
         display: flex;
         flex-direction: column;
     }
+
+    sp-field-label {
+        margin-bottom: 0px;
+    }
+
+    sp-textfield {
+        margin-bottom: 20px;
+        margin-top: -15px;
+    }
 `;
